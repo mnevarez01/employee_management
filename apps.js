e6a334b7cbf029926ee93644cd7f9d2ca798abea
Maria Nevarez <mnevarez1220@gmail.com>
2020-04-25 17:31:04 -0600

    added starter functions to file

--- apps.js
+++ apps.js
@@ -6,4 +6,37 @@ const connection = require("./connection.js");
 connection.connect((err) => {
     if (err) throw err;
     console.log(`Connected as ID: ${connection.threadId}`);
-}) 
+    startMenu()
+})
+
+const startMenu = () => {
+
+}
+
+const addDepartment = () => {
+
+}
+
+const addRole = () => {
+
+}
+
+const addEmployee = () => {
+
+}
+
+const viewDepartment = () => {
+
+}
+
+const viewRole = () => {
+
+}
+
+const viewEmployee = () => {
+
+}
+
+const updateEmployee = () => {
+
+}
