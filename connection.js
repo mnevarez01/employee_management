9fc3e0a5a0dc27599d5b25fd6a02ac3a483d3821
Maria Nevarez <mnevarez1220@gmail.com>
2020-04-25 16:01:30 -0600

    updated connection.js

--- connection.js
+++ connection.js
@@ -1,3 +1,13 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "IRene060464!",
+    port: 3306,
+    database: "top_songsdb"
+})
 
 
 
