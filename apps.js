6ffb723cd1b717cc9b82a3fec260935d67ffa2f8
Maria Nevarez <mnevarez1220@gmail.com>
2020-04-25 17:27:48 -0600

    moved starter table code into folder

--- apps.js
+++ apps.js
@@ -1,3 +1,9 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
 const conTable = require("console.table");
+const connection = require("./connection.js");
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log(`Connected as ID: ${connection.threadId}`);
+}) 
