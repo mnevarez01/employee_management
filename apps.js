39d1cbe94fe55b0c2be74b5d9439b07c90dc5d01
Maria Nevarez <mnevarez1220@gmail.com>
2020-04-25 17:42:39 -0600

    udpated startMenu function

--- apps.js
+++ apps.js
@@ -10,6 +10,42 @@ connection.connect((err) => {
 })
 
 const startMenu = () => {
+    inquirer.prompt(
+        {
+            name: "action",
+            type: "rawlist",
+            message: "What would you like to do?",
+            choices: ["Add a Department", "Add a Role", "Add an Employee", "View Department List", "View Role List", "View Employee List", "Update an Employee Role"]
+        }
+    ).then(({ action }) => {
+        switch (action) {
+            case "Add a Department":
+                addDepartment();
+                break;
+            case "Add a Role":
+                addRole();
+                break;
+            case "Add a Employee":
+                addEmployee();
+                break;
+            case "View Department List":
+                viewDepartment();
+                break;
+            case "View Role List":
+                viewRole();
+                break;
+            case "View Employee List":
+                viewEmployee();
+                break;
+            case "Update an Employee Role":
+                updateEmployee();
+                break;
+
+        }
+
+
+    })
+
 
 }
 
